11a9d2034ed63184b3b853053697a618e33755a0
Saborknight <support@saborknight.com>
2019-02-18 02:54:48 +1100

    Fixed - Custom patches code & `2bnb-logo` image
    What
    =================
    Fixed: Class name that started with a number (Cmon Ford, class names
    must never start with a number or symbol!).
    Fixed: New prefix file and system from
    9bc3f9e4c6caa84c2997901cd2897c5f0ce13ec3 not implemented into patches
    addon.
    Fixed: 2BNB logo cropped incorrectly. It was the old image ripped from
    Arma 3 Unit, instead of the new ones that is actually square and the
    appropriate size.
    
    Issue ID: N/A

--- addons/custom_patches/config.cpp
+++ addons/custom_patches/config.cpp
@@ -17,123 +17,115 @@ class CfgUnitInsignia
 	{
 		displayName="Kompis";
 		author="Ford";
-		texture="\custom_patches\img\Kompis.paa";
+		texture="x\bnb_e\custom_patches\img\Kompis.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class jepulis
 	{
 		displayName="jepulis";
 		author="Ellku";
-		texture="\custom_patches\img\jepulis.paa";
+		texture="x\bnb_e\custom_patches\img\jepulis.paa";
 		textureVehicle="";
-	};	
-	
-	class Kompis
-	{
-		displayName="Kompis";
-		author="Ford";
-		texture="\custom_patches\img\Kompis.paa";
-		textureVehicle="";
-	};	
-	
+	};
+
 	class Punisher
 	{
 		displayName="Punisher";
 		author="Ford";
-		texture="\custom_patches\img\Punisher.paa";
+		texture="x\bnb_e\custom_patches\img\Punisher.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Rball
 	{
 		displayName="Rball";
 		author="Ford";
-		texture="\custom_patches\img\Rball.paa";
+		texture="x\bnb_e\custom_patches\img\Rball.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class SC
 	{
 		displayName="SC";
 		author="Ford";
-		texture="\custom_patches\img\SC.paa";
+		texture="x\bnb_e\custom_patches\img\SC.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Trump
 	{
 		displayName="Trump";
 		author="Ford";
-		texture="\custom_patches\img\Trump.paa";
+		texture="x\bnb_e\custom_patches\img\Trump.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Ussr
 	{
 		displayName="Ussr";
 		author="Ford";
-		texture="\custom_patches\img\Ussr.paa";
+		texture="x\bnb_e\custom_patches\img\Ussr.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Ussr1
 	{
 		displayName="Ussr1";
 		author="Ford";
-		texture="\custom_patches\img\Ussr1.paa";
+		texture="x\bnb_e\custom_patches\img\Ussr1.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Ussr2
 	{
 		displayName="Ussr2";
 		author="Ford";
-		texture="\custom_patches\img\Ussr2.paa";
+		texture="x\bnb_e\custom_patches\img\Ussr2.paa";
 		textureVehicle="";
-	};	
-	
-	class 2BNB
+	};
+
+	class _2BNB
 	{
 		displayName="2BNB";
 		author="2BNB";
-		texture="\custom_patches\img\2BNB.paa";
+		texture="x\bnb_e\custom_patches\img\2bnb-logo.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Putin
 	{
 		displayName="Putin";
 		author="Ford";
-		texture="\custom_patches\img\Putin.paa";
+		texture="x\bnb_e\custom_patches\img\Putin.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Putin1
 	{
 		displayName="Putin1";
 		author="Ford";
-		texture="\custom_patches\img\Putin1.paa";
+		texture="x\bnb_e\custom_patches\img\Putin1.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Canada
 	{
 		displayName="Canada";
 		author="Ford";
-		texture="\custom_patches\img\Canada.paa";
+		texture="x\bnb_e\custom_patches\img\Canada.paa";
 		textureVehicle="";
-	};	
-	
+	};
+
 	class Australia
 	{
 		displayName="Australia";
 		author="Ford";
-		texture="\custom_patches\img\Australia.paa";
+		texture="x\bnb_e\custom_patches\img\Australia.paa";
 		textureVehicle="";
-	};	
-	
-	
-	
+	};
+
+
+
 };
 

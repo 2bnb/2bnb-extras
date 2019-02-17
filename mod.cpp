d75c81b0e3a35d1a8d3515704dedf4265698870d
Saborknight <support@saborknight.com>
2019-02-18 00:40:36 +1100

    Added/Fixed - Logo not displaying properly/cropped badly
    Issue ID: N/A

--- mod.cpp
+++ mod.cpp
@@ -3,15 +3,15 @@ actionName = "Discord";
 action = "https://discord.gg/DRaWNyf";
 description = "2nd Battalion, Nord Brigade - Extras";
 
-picture = "x\bnb_e\core\2bnb-logo.paa";
-contentBrowserPicture="x\bnb_e\core\2bnb-logo.paa";
-logo = "x\bnb_e\core\2bnb-logo.paa";
-logoOver = "x\bnb_e\core\2bnb-logo.paa";
+picture = "2bnb-logo.paa";
+contentBrowserPicture="2bnb-logo.paa";
+logo = "2bnb-logo.paa";
+logoOver = "2bnb-logo.paa";
 
 tooltip = "2nd Battalion, Nord Brigade - Extras";
 tooltipOwned = "2nd Battalion, Nord Brigade - Extras";
 overview = "2BNB extra customisations and enhancements.";
 author = "2nd Battalion, Nord Brigade Technical Team";
-overviewPicture = "x\bnb_e\core\2bnb-logo.paa";
+overviewPicture = "2bnb-logo.paa";
 overviewText = "2nd Battalion, Nord Brigade - Extras";
 overviewFootnote = "<br /><br /><t color='#999999'>This content is under APL-SA License.<t />";

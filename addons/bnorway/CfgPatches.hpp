1e0780a97a42a368a225b9719fefeb9c9ecfc25a
GhostTheSlayer <GhostTheSlayer@users.noreply.github.com>
2019-03-09 23:00:46 +0100

    Custom Norway army units
    Preview dosen't work, merge all custom units into one when I get time... Also added 3 pictures for #8

---
+++ addons/bnorway/CfgPatches.hpp
@@ -0,0 +1,87 @@
+class CfgPatches {
+    class B_Norway {
+        units[] = {
+            "B_Norway_IC_2IC_01",
+            "B_Norway_M81_AT_01",
+            "B_Norway_M81_LMG_01",
+            "B_Norway_M81_Marksman_01",
+            "B_Norway_M81_Rifleman_01",
+            "B_Norway_M81_Medic_01",
+            "B_Norway_AOR1_IC_2IC_01",
+            "B_Norway_AOR1_AT_01",
+            "B_Norway_AOR1_Marksman_01",
+            "B_Norway_AOR1_Rifleman_01",
+            "B_Norway_AOR1_Medic_01",
+            "B_Norway_AOR_LMG_01",
+            "B_Norway_M81_Crew_01",
+            "B_Norway_M81_Air_Crew_01",
+            "B_Norway_AOR1_Crew_01",
+            "B_Norway_AOR1_Air_Crew_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F"
+        };
+        author = "Maj. D. Ford";
+        authors[] = {"Maj. D. Ford"};
+    };
+};
+class CfgPatches {
+    class B_Norway {
+        units[] = {
+            "B_Norway_IC_2IC_01",
+            "B_Norway_M81_AT_01",
+            "B_Norway_M81_LMG_01",
+            "B_Norway_M81_Marksman_01",
+            "B_Norway_M81_Rifleman_01",
+            "B_Norway_M81_Medic_01",
+            "B_Norway_AOR1_IC_2IC_01",
+            "B_Norway_AOR1_AT_01",
+            "B_Norway_AOR1_Marksman_01",
+            "B_Norway_AOR1_Rifleman_01",
+            "B_Norway_AOR1_Medic_01",
+            "B_Norway_AOR_LMG_01",
+            "B_Norway_M81_Crew_01",
+            "B_Norway_M81_Air_Crew_01",
+            "B_Norway_AOR1_Crew_01",
+            "B_Norway_AOR1_Air_Crew_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F"
+        };
+        author = "Maj. D. Ford";
+        authors[] = {"Maj. D. Ford"};
+    };
+};
+class CfgPatches {
+    class B_Norway {
+        units[] = {
+            "B_Norway_IC_2IC_01",
+            "B_Norway_M81_AT_01",
+            "B_Norway_M81_LMG_01",
+            "B_Norway_M81_Marksman_01",
+            "B_Norway_M81_Rifleman_01",
+            "B_Norway_M81_Medic_01",
+            "B_Norway_AOR1_IC_2IC_01",
+            "B_Norway_AOR1_AT_01",
+            "B_Norway_AOR1_Marksman_01",
+            "B_Norway_AOR1_Rifleman_01",
+            "B_Norway_AOR1_Medic_01",
+            "B_Norway_AOR_LMG_01",
+            "B_Norway_M81_Crew_01",
+            "B_Norway_M81_Air_Crew_01",
+            "B_Norway_AOR1_Crew_01",
+            "B_Norway_AOR1_Air_Crew_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F"
+        };
+        author = "Maj. D. Ford";
+        authors[] = {"Maj. D. Ford"};
+    };
+};

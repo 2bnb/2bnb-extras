5525257b7e3ecb435b51024c7e4fcaa932ab6e1b
Saborknight <support@saborknight.com>
2019-05-08 22:48:51 +1000

    Changed - Norwegian Army faction and units with updated loadouts
    What
    =================
    Changed: Updated the units with their new default loadouts, also
    re-organised the groups and the code to be cleaner.
    Fixed: Abilities and role icons not being implemented depending on the
    units role (I.e. Medic, Engineer/EOD, etc.)
    
    WARNING: Do not simply copy/paste what the ORBAT creator gives you. Only
    paste in the loadout changes from the ORBAT creator. If you want to add
    more units/groups, or change existing ones, do that manually instead of
    a mass paste... Otherwise you'll break the above mentioned fixes and
    changes.
    
    Issue ID: N/A

--- addons/bnorway/CfgPatches.hpp
+++ addons/bnorway/CfgPatches.hpp
@@ -1,30 +1,37 @@
 class CfgPatches {
-	class B_Norway {
+	class B_NorwayArmy {
 		name = "2BNB Extras - Norway Units";
-		units[] = {
-			"B_Norway_IC_2IC_01",
-			"B_Norway_M81_AT_01",
-			"B_Norway_M81_LMG_01",
-			"B_Norway_M81_Marksman_01",
-			"B_Norway_M81_Rifleman_01",
-			"B_Norway_M81_Medic_01",
-			"B_Norway_AOR1_IC_2IC_01",
-			"B_Norway_AOR1_AT_01",
-			"B_Norway_AOR1_Marksman_01",
-			"B_Norway_AOR1_Rifleman_01",
-			"B_Norway_AOR1_Medic_01",
-			"B_Norway_AOR_LMG_01",
-			"B_Norway_M81_Crew_01",
-			"B_Norway_M81_Air_Crew_01",
-			"B_Norway_AOR1_Crew_01",
-			"B_Norway_AOR1_Air_Crew_01"
-		};
+        units[] = {
+            "B_NorwayArmy_Rifleman_Woodland_01",
+            "B_NorwayArmy_Rifleman_AT_Woodland_01",
+            "B_NorwayArmy_Engineer_EOD_Woodland_01",
+            "B_NorwayArmy_1IC_Woodland_01",
+            "B_NorwayArmy_2IC_Woodland_01",
+            "B_NorwayArmy_Marksman_Woodland_01",
+            "B_NorwayArmy_LMG_556_Woodland_01",
+            "B_NorwayArmy_LMG_762_Woodland_01",
+            "B_NorwayArmy_Medic_Woodland_01",
+            "B_NorwayArmy_Man_Woodland_01",
+            "B_NorwayArmy_Man_Desert_01",
+            "B_NorwayArmy_Medic_Desert_01",
+            "B_NorwayArmy_LMG_762_Desert_01",
+            "B_NorwayArmy_LMG_556_Desert_01",
+            "B_NorwayArmy_Marksman_Desert_01",
+            "B_NorwayArmy_Engineer_EOD_Desert_01",
+            "B_NorwayArmy_1IC_Desert_01",
+            "B_NorwayArmy_2IC_Desert_01",
+            "B_NorwayArmy_Rifleman_AT_Desert_01",
+            "B_NorwayArmy_Rifleman_Desert_01"
+        };
 		weapons[] = {};
 		requiredVersion = 1.62;
 		requiredAddons[] = {
 			"A3_Characters_F"
 		};
 		author = "Maj. D. Ford";
-		authors[] = {"Maj. D. Ford"};
+		authors[] = {
+			"Maj. D. Ford",
+			"Capt. Arend"
+		};
 	};
 };
--- addons/bnorway/config.cpp
+++ addons/bnorway/config.cpp
@@ -1,5 +1,5 @@
-// #include "script_component.hpp"
 #include "CfgPatches.hpp"
+#include "CfgWorlds.hpp"
 #include "CfgFactionClasses.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgGroups.hpp"

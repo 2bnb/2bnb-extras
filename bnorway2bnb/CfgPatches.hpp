c9b7bd97f715553b49b04f2a775735c25ec931df
Papagon2 <50224858+Papagon2@users.noreply.github.com>
2019-05-27 00:47:20 +0200

    New/edited Norwegian Faction
    Made new Folder: "bnorway2" It contains the Norwegian Faction with Car's, APC, Tank, Planes, Helicopter and Static. This is crated with ORBAT in ALIVE MOD.
    PS: Arend plz check this out or the one with ALIVE knowlage.

---
+++ bnorway2bnb/CfgPatches.hpp
@@ -0,0 +1,51 @@
+class CfgPatches {
+    class B_Norway2BNB {
+        units[] = {
+            "B_Norway2BNB_LMG_762_01",
+            "B_Norway2BNB_Man_01",
+            "B_Norway2BNB_Rifleman_01",
+            "B_Norway2BNB_Rifleman_AT_01",
+            "B_Norway2BNB_Engineer_EOD_01",
+            "B_Norway2BNB_1IC_01",
+            "B_Norway2BNB_2IC_01",
+            "B_Norway2BNB_Marksman_01",
+            "B_Norway2BNB_LMG_556_01",
+            "B_Norway2BNB_Medic_01",
+            "B_Norway2BNB_Jackal_2HMG_Wd",
+            "B_Norway2BNB_Crew_01",
+            "B_Norway2BNB_LRover_HardTop_FFR_G",
+            "B_Norway2BNB_LRover_WMIK_HMG_FFR_G",
+            "B_Norway2BNB_MAN_HX60_4x4_Fuel_G",
+            "B_Norway2BNB_MAN_HX60_4x4_Repair_G",
+            "B_Norway2BNB_MAN_HX60_4x4_Transport_G",
+            "B_Norway2BNB_Panther_CLV_GPMG_G",
+            "B_Norway2BNB_FV432_3Bulldog_HMG_RWS_G",
+            "B_Norway2BNB_FV432_3Bulldog_GPMG_G",
+            "B_Norway2BNB_Leopard_2SG",
+            "B_Norway2BNB_Wildcat_AH1_8CAS",
+            "B_Norway2BNB_Pilot",
+            "B_Norway2BNB_Wildcat_AH18_Transport",
+            "B_Norway2BNB_Merlin_HC3_18",
+            "B_Norway2BNB_Chinook_HC2",
+            "B_Norway2BNB_Apache_AH1_Multi",
+            "B_Norway2BNB_Hercules_C4",
+            "B_Norway2BNB_L18_101_CAP",
+            "B_Norway2BNB_L111A1_H",
+            "B_Norway2BNB_L134A1_High",
+            "B_Norway2BNB_L16",
+            "B_Norway2BNB_L7A2_High"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "B_NorwayArmy",
+            "UK3CB_BAF_Units_Tropical",
+            "A3_Armor_F_EPB_MBT_03",
+            "UK3CB_BAF_Vehicles_Chinook",
+            "UK3CB_BAF_Vehicles_Hercules",
+            "rhs_c_mig29"
+        };
+        author = "Jebby";
+        authors[] = {"Jebby"};
+    };
+};
---
+++ bnorway2bnb/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT bnorway2bnb
+#define COMPONENT_BEAUTIFIED bnorway2bnb
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_BNORWAY2BNB
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_BNORWAY2BNB
+#define DEBUG_SETTINGS DEBUG_SETTINGS_BNORWAY2BNB
+#endif
+#include "\x\\addons\main\script_macros.hpp"

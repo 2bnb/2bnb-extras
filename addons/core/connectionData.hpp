89ad3603b9923f1ba6f2846b32e2fc39ff999216
Saborknight <support@saborknight.com>
2019-09-06 17:41:57 +1000

    Changed/Removed - Dev server tile to Public server + connection sample
    Why
    =================
    Dev server is seldom up, and not relevant to all our players. The public
    server is and is relevant.
    Since we added the `connectionData.hpp` file to the repo proper, there's
    no need for the sample file.
    
    Issue ID: N/A

--- addons/core/connectionData.hpp
+++ addons/core/connectionData.hpp
@@ -1,9 +1,9 @@
-class bnb_e_dev_server {
-	text = "2BNB Dev Server";
+class bnb_e_public_server {
+	text = "2BNB Public Server";
 	textIsQuote = 0;
 	picture = "\x\bnb_e\core\2bnb-tile.paa";
 	video = "\x\bnb_e\core\2bnb-tile.ogv";
-	action = "[""5.9.108.39"", 2312] call compile preprocessFileLineNumbers '\x\bnb_e\core\functions\direct_connect\fn_connectToServer.sqf'";
+	action = "[""5.9.108.39"", 2350] call compile preprocessFileLineNumbers '\x\bnb_e\core\functions\direct_connect\fn_connectToServer.sqf'";
 	actionText = "Connect";
 	condition = true;
 };

6db2b5e89453540b7d293f0b397a402b8ecadc8d
Saborknight <support@saborknight.com>
2019-11-15 18:04:23 +1100

    Fixed - ACE "Swap Barrel" not working for some LMGs
    Issue ID: #59

--- addons/core/config.cpp
+++ addons/core/config.cpp
@@ -69,6 +69,20 @@ class CfgWeapons {
 	class tfw_rf3080Item: ACE_ItemCore {
 		class ItemInfo: CBA_MiscItem_ItemInfo{};
 	};
+
+	// ACE Barrel Swap fixes
+	class Rifle_Long_Base_F;
+	class UK3CB_BAF_L110_Base : Rifle_Long_Base_F {
+		ace_overheating_allowSwapBarrel = 1;
+	};
+	class UK3CB_BAF_L7A2 : Rifle_Long_Base_F {
+		ace_overheating_allowSwapBarrel = 1;
+	};
+
+	class LMG_03_base_F;
+	class LMG_03_F : LMG_03_base_F {
+		ace_overheating_allowSwapBarrel = 1;
+	};
 };
 
 class CfgMagazines {

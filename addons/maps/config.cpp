c984f27ceaa8c9078d5f5770c1f9af07cbf5ec53
Saborknight <support@saborknight.com>
2020-04-14 12:49:27 +1000

    Fixed - Chernarus Winter map now has Northing fixed
    Issue ID: #95

--- addons/maps/config.cpp
+++ addons/maps/config.cpp
@@ -3,6 +3,7 @@ class CfgPatches {
 		name = "2BNB Extras - Maps";
 		weapons[] = {};
 		requiredAddons[] = {
+			"CUP_Chernarus_Winter_Config"
 		};
 		author = "2nd Battalion, Nord Brigade";
 		authors[] = {
@@ -16,5 +17,6 @@ class CfgWorlds {
 	// Find the classnames in the `altis_map.pbo` file
 	// For other maps, look in the same place but for the  respective mmap
 	// See: https://github.com/2bnb/2bnb-extras/wiki/Renaming-Map-Locations
+	#include "chernarus_winter.hpp";
 	#include "altis.hpp";
 };

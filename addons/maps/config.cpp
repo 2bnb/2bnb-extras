4bb80918985c37a7172756feecfd9ae0d396c0d2
Saborknight <support@saborknight.com>
2020-10-14 15:11:01 +1100

    Removed - Chernarus Winter Grid coordinate fix
    Why
    =================
    CUP Terrains - Maps has now fixed it in the original mod.
    
    Issue ID: #95

--- addons/maps/config.cpp
+++ addons/maps/config.cpp
@@ -3,9 +3,7 @@ class CfgPatches {
 		name = "2BNB Extras - Maps";
 		units[] = {};
 		weapons[] = {};
-		requiredAddons[] = {
-			"CUP_Chernarus_Winter_Config"
-		};
+		requiredAddons[] = {};
 		author = "2nd Battalion, Nord Brigade";
 		authors[] = {
 			"Arend"
@@ -18,6 +16,5 @@ class CfgWorlds {
 	// Find the classnames in the `altis_map.pbo` file
 	// For other maps, look in the same place but for the  respective mmap
 	// See: https://github.com/2bnb/2bnb-extras/wiki/Renaming-Map-Locations
-	#include "chernarus_winter.hpp";
 	#include "altis.hpp";
 };

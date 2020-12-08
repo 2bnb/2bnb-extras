07e69d279897c75f9e3547843ddb50ee6bd94235
Saborknight <support@saborknight.com>
2020-12-08 14:47:33 +1100

    Added - BettIR Compatibility for NORSOF NVGs
    NORSOF NVGs now have IR lights on them.
    
    Issue ID: N/A

---
+++ addons/bettir_compat/config.cpp
@@ -0,0 +1,39 @@
+class CfgPatches {
+	class bnb_ex_bettir_compat {
+		// Addon identity
+		name = "2BNB Extras - BettIR Compat";
+		author = "2nd Battalion, Nord Brigade";
+		authors[] = {
+			"Arend"
+		};
+		url = "https://discord.gg/DRaWNyf";
+		version = 1.0.0;
+		versionAr[] = {1,0,0,0};
+		versionStr = "v1.0.0";
+
+		// Addon requirements
+		requiredVersion = 2.00;
+		requiredAddons[] = {};
+
+		// Addon items
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		worlds[] = {};
+	};
+};
+
+class BettIR_Config {
+	class CompatibleNightvisionGoggles {
+		 class NVGoggles_mas_nor_h {
+			offset[]={-0.027000001,0.28,0.15000001};
+		};
+		 class NVGoggles_mas_nor_hv : NVGoggles_mas_nor_h {
+			offset[]={-0.027000001,0.28,0.15000001};
+		};
+		 class NVGoggles_mas_nor_hr : NVGoggles_mas_nor_h {
+			offset[]={-0.027000001,0.28,0.15000001};
+		};
+	};
+};

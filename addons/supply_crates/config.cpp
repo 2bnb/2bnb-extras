70080ac72574b1208824c81f32f9672db12886da
Alex R <alex_farcry@hotmail.co.uk>
2023-06-22 19:14:31 +0100

    config.cpp overwrite

--- addons/supply_crates/config.cpp
+++ addons/supply_crates/config.cpp
@@ -1,10 +1,11 @@
 class CfgPatches {
 	class bnb_ex_supply_crates {
 		// Addon identity
-		name = "TF15 Extras - Supply Crates";
-		author = "Task Force 15";
+		name = "2BNB - Supply Crates";
+		author = "2BNB";
 		authors[] = {
-			"Arend"
+			"Arend",
+			"FarCry"
 		};
 		url = "https://discord.gg/rUUsCShkzJ";
 		version = 1.0.0;
@@ -17,9 +18,6 @@ class CfgPatches {
 			"ace_medical_treatment",
 			"ace_grenades",
 			"ace_explosives",
-			"free_world_armoury",
-			"japan_units",
-			"rhsusaf",
 			"CUP_Weapons_Ammunition"
 		};
 
@@ -57,24 +55,20 @@ class CfgVehicles {
 	};
 
 	class BNB_EX_Resupply_Class_A : BNB_EX_Resupply {
-<<<<<<< Updated upstream
-		displayName = "[TF15] Resupply Crate (Medic) - Class A";
-=======
 		displayName = "[2BNB] Medical Crate";
->>>>>>> Stashed changes
 		scope = 2;
 
 		class TransportItems {
-			class _xx_ACE_plasmaIV {
-				name = "ACE_plasmaIV";
+			class _xx_ACE_bloodIV {
+				name = "ACE_bloodIV";
 				count = 10;
 			};
-			class _xx_ACE_plasmaIV_500 {
-				name = "ACE_plasmaIV_500";
+			class _xx_ACE_bloodIV_500 {
+				name = "ACE_bloodIV_500";
 				count = 10;
 			};
-			class _xx_ACE_plasmaIV_250 {
-				name = "ACE_plasmaIV_250";
+			class _xx_ACE_bloodIV_250 {
+				name = "ACE_bloodIV_250";
 				count = 10;
 			};
 			class _xx_ACE_elasticBandage {
@@ -113,11 +107,7 @@ class CfgVehicles {
 	};
 
 	class BNB_EX_Resupply_Class_B : BNB_EX_Resupply {
-<<<<<<< Updated upstream
-		displayName = "[TF15] Resupply Crate (Ammo) - Class B";
-=======
 		displayName = "[2BNB] Ammo Crate";
->>>>>>> Stashed changes
 		scope = 2;
 
 		class TransportMagazines {
@@ -133,129 +123,30 @@ class CfgVehicles {
 				magazine = "ACE_30Rnd_556x45_Stanag_Dim";
 				count = 15;
 			};
-<<<<<<< Updated upstream
-			class _xx_CUP_30Rnd_556x45_PMAG_BLACK {
-				magazine = "CUP_30Rnd_556x45_PMAG_BLACK";
-				count = 15
-			};
-=======
->>>>>>> Stashed changes
 			class _xx_rhs_mag_30Rnd_556x45_Mk318_PMAG {
 				magazine = "rhs_mag_30Rnd_556x45_Mk318_PMAG";
-				count = 15
+				count = 15;
 			};
 			class _xx_rhs_mag_30Rnd_556x45_Mk262_PMAG {
 				magazine = "rhs_mag_30Rnd_556x45_Mk262_PMAG";
-				count = 15
+				count = 15;
 			};
 			class _xx_rhs_mag_30Rnd_556x45_M855A1_PMAG_Tacer_Red {
 				magazine = "rhs_mag_30Rnd_556x45_M855A1_PMAG_Tacer_Red";
-				count = 15
+				count = 15;
 			};
 			class _xx_rhs_mag_30Rnd_556x45_M855_PMAG_Tracer_Red {
 				magazine = "rhs_mag_30Rnd_556x45_M855_PMAG_Tracer_Red";
-				count = 15
+				count = 15;
 			};
-<<<<<<< Updated upstream
-			class _xx_30Rnd_jpa_556x45_T_Stanag {
-				magazine = "30Rnd_jpa_556x45_T_Stanag";
-				count = 15
-			};
-			class _xx_20Rnd_jpa_762x51_Stanag {
-				magazine = "20Rnd_jpa_762x51_Stanag";
-				count = 10
-			};
-			class _xx_20Rnd_jpa_762x51_T_Stanag {
-				magazine = "20Rnd_jpa_762x51_T_Stanag";
-				count = 10
-			};
-=======
->>>>>>> Stashed changes
 			class _xx_ACE_20Rnd_762x51_M993_AP_Mag {
 				magazine = "ACE_20Rnd_762x51_M993_AP_Mag";
-				count = 10
+				count = 10;
 			};
 			class _xx_ACE_20Rnd_762x51_Mag_Tracer_Dim {
 				magazine = "ACE_20Rnd_762x51_Mag_Tracer_Dim";
-				count = 10
+				count = 10;
 			};
-			class _xx_sp_fwa_m67_1rnd_heat {
-				magazine = "sp_fwa_m67_1rnd_heat";
-				count = 3
-			};
-			class _xx_sp_fwa_m67_1rnd_he {
-				magazine = "sp_fwa_m67_1rnd_he";
-				count = 3
-			};
-<<<<<<< Updated upstream
-			class _xx_sp_fwa_100Rnd_762_mag_ball {
-				magazine = "sp_fwa_100Rnd_762_mag_ball";
-				count = 6
-			};
-			class _xx_CUP_lmg_m249_pip3 {
-				magazine = "CUP_lmg_m249_pip3";
-				count = 8
-			};
-			class _xx_CUP_100Rnd_TE4_Red_Tracer_556x45_M249 {
-				magazine = "CUP_100Rnd_TE4_Red_Tracer_556x45_M249";
-				count = 8
-			};
-			class _xx_CUP_lmg_M240_B {
-				magazine = "CUP_lmg_M240_B";
-				count = 6
-			};
-			class _xx_CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M {
-				magazine = "CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M";
-				count = 6
-			};
-			class _xx_rhsusf_100Rnd_762x51_m61_ap {
-				magazine = "rhsusf_100Rnd_762x51_m61_ap";
-				count = 6
-			};
-			class _xx_rhsusf_100Rnd_762x51_m62_tracer {
-				magazine = "rhsusf_100Rnd_762x51_m62_tracer";
-				count = 6
-			};
-			class _xx_rhsusf_100Rnd_762x51_m80a1epr {
-				magazine = "rhsusf_100Rnd_762x51_m80a1epr";
-				count = 6
-			};
-			class _xx_rhsusf_100Rnd_556x45_soft_pouch_coyote {
-				magazine = "rhsusf_100Rnd_556x45_soft_pouch_coyote";
-				count = 8
-			};
-			class _xx_rhsusf_100Rnd_556x45_mixed_soft_pouch_coyote {
-				magazine = "rhsusf_100Rnd_556x45_mixed_soft_pouch_coyote";
-				count = 8
-			};
-			class _xx_rhsusf_100Rnd_556x45_M995_soft_pouch_coyote {
-				magazine = "rhsusf_100Rnd_556x45_M995_soft_pouch_coyote";
-				count - 8
-			};
-			class _xx_10Rnd_338_Mag {
-				magazine = "10Rnd_338_Mag";
-				count = 8
-			};
-			class _xx_ACE_10Rnd_762x67_Berger_Hybrid_OTM_Mag {
-				magazine = "ACE_10Rnd_762x67_Berger_Hybrid_OTM_Mag";
-				count = 8
-			};
-			class _xx_rhs_fim92_mag {
-				magazine = "rhs_fim92_mag";
-				count = 2
-			};
-			class _xx_sp_fwa_20Rnd_762_FAL_Metric_Tracer {
-				magazine = "sp_fwa_20Rnd_762_FAL_Metric_Tracer";
-				count = 10
-			};
-			class _xx_sp_fwa_20Rnd_762_FAL_Metric {
-				magazine = "sp_fwa_20Rnd_762_FAL_Metric";
-				count = 10
-			};
-			class _xx_sp_fwa_20Rnd_762_FAL_Metric_Ball {
-				magazine = "sp_fwa_20Rnd_762_FAL_Metric_Ball";
-				count = 10
-=======
 			class _xx_UK3CB_BAF_556_200Rnd_T {
 				magazine = "UK3CB_BAF_556_200Rnd_T";
 				count = 10;
@@ -303,7 +194,6 @@ class CfgVehicles {
 			class _xx_UK3CB_BAF_556_30Rnd_T {
 				magazine = "UK3CB_BAF_556_30Rnd_T";
 				count = 15;
->>>>>>> Stashed changes
 			};
 			class _xx_ACE_HuntIR_M203 {
 				magazine = "ACE_HuntIR_M203";
@@ -345,15 +235,15 @@ class CfgVehicles {
 				weapon = "hgun_Pistol_Signal_F";
 				count = 2;
 			};
+			class _xx_rhs_weap_m72a7 {
+				weapon = "rhs_weap_m72a7";
+				count = 4;
+			};
 		};
 	};
 
 	class BNB_EX_Resupply_Class_C : BNB_EX_Resupply {
-<<<<<<< Updated upstream
-		displayName = "[TF15] Resupply Crate (Explosives) - Class C";
-=======
 		displayName = "[2BNB] Explosive Crate";
->>>>>>> Stashed changes
 		scope = 2;
 
 		class TransportMagazines {
